d089309cebfa8ef594a9f03fea9516597049491e
Kajukatli <mandar@Apples-MacBook.local>
2022-11-29 16:26:18 +0530

    view contact,search contact,exit

--- Contact.h
+++ Contact.h
@@ -24,7 +24,7 @@ Contact(int cid,const char* cname ,const char* cmobile,const char* cemail ,const
     strcpy(addr,caddr);
     strcpy(fbid,cfbid);
 }
-
+//to display single contact
 void disp(){
     cout<<"ID: "<<id<<endl;
     cout<<"Name: "<<name<<endl;
@@ -34,11 +34,60 @@ void disp(){
     cout<<"FbID: "<<fbid<<endl;
     cout<<"================================================\n"<<endl;
 }
+friend void search(int id);
+friend void deleted(int id);
 };
-
+//to add contcat
 void addContact(Contact c){
     ofstream fout ("contact.dat",ios::app);
     fout.write((char*)&c,sizeof(c));
     fout.close();
     cout<<"Contact added sucessfully"<<endl;
-}
+};
+
+//to display all contacts
+void dispAll(){
+    Contact temp;
+    ifstream inf("contact.dat");
+    while(1){
+        inf.read((char*)&temp,sizeof(temp));
+        if(inf.eof()){
+            break;
+        }else{
+            temp.disp();
+        }
+    }
+};
+
+//to search contacts
+void search(int id){
+    Contact temp;
+    ifstream inf("contact.dat");
+    while(1){
+        inf.read((char*)&temp,sizeof(temp));
+        if(inf.eof()){
+            break;
+        }
+        else if(temp.id == id){
+            cout<<"Record found:"<<endl;
+                temp.disp();
+            }
+    }
+    cout<<"No contact found!!\n";
+};
+
+
+//to delete contact
+void deleted(int id){
+    Contact temp;
+    ifstream inf("contact.dat");
+    inf.read((char*)&temp,sizeof(temp));
+    while(1){
+        if(inf.eof()){
+            break;
+        }
+        if(temp.id == id){
+
+        }
+    }
+};
--- main.cpp
+++ main.cpp
@@ -2,19 +2,20 @@
 
 int main(){
     int ch;
-
+while (1)
+    {
     Contact c(1,"macw","3737373783","hm@gamil.com","kolhpur","fb/apk");
-    cout<<"==============================="<<endl;
+    cout<<"*******************"<<endl;
     cout<<"Enter the choise:"<<endl;
     cout<<"1.Add contact"<<endl;
     cout<<"2.View all contact"<<endl;
     cout<<"3.Search contact"<<endl;
     cout<<"4.Delete contact"<<endl;
     cout<<"5.Exit"<<endl;
-    cout<<"==============================="<<endl;
+    cout<<"*******************"<<endl;
     cin>>ch;
 
-    switch(ch){
+        switch(ch){
         case 1:{
             int id;
             char name[20],mobile[20],email[20],addr[40],fbid[20];
@@ -35,9 +36,33 @@ int main(){
             Contact c(id,name,mobile,email,addr,fbid);
             addContact(c);
         }
+        break;
+
         case 2:{
-            // c.disp();
+            dispAll();
+        }
+        break;
+
+        case 3:{
+            int id;
+            cout<<"Enter ID want to search:"<<endl;
+            cin>>id;
+            search(id);
+        }
+        break;
+
+        case 4:{
+            int id;
+            cout<<"Enter ID want to DELETE:"<<endl;
+            cin>>id;
+            deleted(id);
+        }
+        break;
+        
+        case 5:{
+            exit(0);
         }
     }
+ }  
     return 0;
 }
